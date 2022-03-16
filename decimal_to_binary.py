2a77ab7414ac06e22b39f69cf79a9ce494650e57
MusaSudad <musasudad271@gmail.com>
2022-03-16 16:17:14 +0600

    some changes

---
+++ decimal_to_binary.py
@@ -0,0 +1,16 @@
+from msilib.schema import Binary
+
+
+num = int(input("Enter your number \n->"))
+decimal = num
+while decimal!=0:
+    updater= int(decimal%2)
+    decimal = decimal/2
+    decimal = updater+updater
+
+print(decimal)
+
+
+ 
+
+
