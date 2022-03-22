7151c7e8fd86802d9ecd0fafa3eb9919ca4fce89
MusaSudad <musasudad271@gmail.com>
2022-03-23 01:58:34 +0600

    some changes🤣

---
+++ code_files/confusion.py
@@ -0,0 +1,7 @@
+my_list = ["chocolate","milk","sugar","cream"]
+#index => roll number
+
+for i in [0,1,2,3]:
+    print(my_list[i])
+    #iterating through a list
+    
---
+++ code_files/hw3.py
@@ -0,0 +1,129 @@
+hex_num = input("Enter hexadecimal numbers with , between bits : ")
+hex_list = hex_num.split(",")
+if hex_list[0] == "1" :
+    hex_list[0] = "0001"
+elif hex_list[0] == "2" :
+    hex_list[0] ="0010"
+elif hex_list[0] == "3" :
+    hex_list[0] =="0011"
+elif hex_list[0] == "4" :
+    hex_list[0]  = "0100"
+elif hex_list[0] == "5" :
+    hex_list[0]  = "0101"
+elif hex_list[0] == "6" :
+    hex_list[0]  = "0110"
+elif hex_list[0] == "7" :
+    hex_list[0]  = "0111"
+elif hex_list[0] == "8" :
+    hex_list[0]  = "1000"
+elif hex_list[0] == "9" :
+    hex_list[0]  = "1001"
+elif hex_list[0] == "A" :
+    hex_list[0]  = "1010"
+elif hex_list[0] == "B" :
+    hex_list[0]  = "1011"
+elif hex_list[0] == "C" :
+    hex_list[0]  = "1100"
+elif hex_list[0] == "D" :
+    hex_list[0]  = "1101"
+elif hex_list[0] == "E" :
+    hex_list[0]  = "1110"
+elif hex_list[0] == "F" :
+    hex_list[0]  = "1111"
+
+
+if hex_list[1] == "1" :
+    hex_list[1] = "0001"
+elif hex_list[1] == "2" :
+    hex_list[1] ="0010"
+elif hex_list[1] == "3" :
+    hex_list[1] =="0011"
+elif hex_list[1] == "4" :
+    hex_list[1]  = "0100"
+elif hex_list[1] == "5" :
+    hex_list[1]  = "0101"
+elif hex_list[1] == "6" :
+    hex_list[1]  = "0110"
+elif hex_list[1] == "7" :
+    hex_list[1]  = "0111"
+elif hex_list[1] == "8" :
+    hex_list[1]  = "1000"
+elif hex_list[1] == "9" :
+    hex_list[1]  = "1001"
+elif hex_list[1] == "A" :
+    hex_list[1]  = "1010"
+elif hex_list[1] == "B" :
+    hex_list[1]  = "1011"
+elif hex_list[1] == "C" :
+    hex_list[1]  = "1100"
+elif hex_list[1] == "D" :
+    hex_list[1]  = "1101"
+elif hex_list[1] == "E" :
+    hex_list[1]  = "1110"
+elif hex_list[1] == "F" :
+    hex_list[1]  = "1111"
+
+if hex_list[2] == "1" :
+    hex_list[2] = "0001"
+elif hex_list[2] == "2" :
+    hex_list[2] ="0010"
+elif hex_list[2] == "3" :
+    hex_list[2] =="0011"
+elif hex_list[2] == "4" :
+    hex_list[2]  = "0100"
+elif hex_list[2] == "5" :
+    hex_list[2]  = "0101"
+elif hex_list[2] == "6" :
+    hex_list[2]  = "0110"
+elif hex_list[2] == "7" :
+    hex_list[2]  = "0111"
+elif hex_list[2] == "8" :
+    hex_list[2]  = "1000"
+elif hex_list[2] == "9" :
+    hex_list[2]  = "1001"
+elif hex_list[2] == "A" :
+    hex_list[2]  = "1010"
+elif hex_list[2] == "B" :
+    hex_list[2]  = "1011"
+elif hex_list[2] == "C" :
+    hex_list[2]  = "1100"
+elif hex_list[2] == "D" :
+    hex_list[2]  = "1101"
+elif hex_list[2] == "E" :
+    hex_list[2]  = "1110"
+elif hex_list[2] == "F" :
+    hex_list[2]  = "1111"
+
+if hex_list[3] == "1" :
+    hex_list[3] = "0001"
+elif hex_list[3] == "2" :
+    hex_list[3] ="0010"
+elif hex_list[3] == "3" :
+    hex_list[3] =="0011"
+elif hex_list[3] == "4" :
+    hex_list[3]  = "0100"
+elif hex_list[3] == "5" :
+    hex_list[3]  = "0101"
+elif hex_list[3] == "6" :
+    hex_list[3]  = "0110"
+elif hex_list[3] == "7" :
+    hex_list[3]  = "0111"
+elif hex_list[3] == "8" :
+    hex_list[3]  = "1000"
+elif hex_list[3] == "9" :
+    hex_list[3]  = "1001"
+elif hex_list[3] == "A" :
+    hex_list[3]  = "1010"
+elif hex_list[3] == "B" :
+    hex_list[3]  = "1011"
+elif hex_list[3] == "C" :
+    hex_list[3]  = "1100"
+elif hex_list[3] == "D" :
+    hex_list[3]  = "1101"
+elif hex_list[3] == "E" :
+    hex_list[3]  = "1110"
+elif hex_list[3] == "F" :
+    hex_list[3]  = "1111"
+
+
+print(hex_list[0]+hex_list[1]+hex_list[2]+hex_list[3])
