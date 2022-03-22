a9e2165bcf14a77627b7c8dcbd6e4176d5a873ed
MusaSudad <musasudad271@gmail.com>
2022-03-22 12:11:08 +0600

    some changes

---
+++ code_files/ex32ph.py
@@ -0,0 +1,11 @@
+the_count = [1,2,3,4,5]
+fruits = ['apples','oranges','pears','apricots']
+change = [1,'pennies',2,'dimes',3,'quaters']
+
+for number in the_count :
+    print(f"This is count {number}")
+
+for number in fruits:
+    print(f"A fruit of type: {number}")
+    
+
